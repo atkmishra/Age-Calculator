32511bd43fe90ae2aee18f53f72342e9318feec1
atkmishra <96171238+atkmishra@users.noreply.github.com>
2022-02-11 20:26:38 +0530

    1st commit

--- Codecamp_project.js
+++ Codecamp_project.js
@@ -99,16 +99,25 @@
 
 function age() {  
     var birthday_Date = document.getElementById('date').value;  
-    var bithday_Month = document.getElementById('month').value;  
+    var birthday_Month = document.getElementById('month').value;  
     var birthday_Year = document.getElementById('year').value;  
     var date = new Date();  
-    var today_Day = date.getDate();  
-    var today_month = date.getMonth();  
+    var today_Date = date.getDate();  
+    var today_Month = date.getMonth();  
     var today_Year = date.getFullYear();  
+    if(birthday_Date>today_Date){
+        
+    }
+    if(today_Month>birthday_Month){
+        calculated_age = today_Year - birthday_Year;
+    }
+    else{
+        claculated_age = today_Year - birthday_Month-1;
+    }
 
       
-    var d = d2 - d1;  
-    var m = m2 - m1;  
-    var y = y2 - y1;  
+    var d = today_Date - birthday_Date;  
+    var m = today_Month - birthday_Month;  
+    var y = today_Year - birthday_Year;  
     document.getElementById('age').innerHTML = 'Your Age is '+y+' Years '+m+' Months '+d+' Days';  
    }  
